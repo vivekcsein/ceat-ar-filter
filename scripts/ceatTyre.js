53382c2e8ac2d396c66f00405202dca5d9ce7f76
Vivek <vivekcse.in@gmail.com>
2024-01-09 23:52:44 +0530

    final commit for the client

--- scripts/ceatTyre.js
+++ scripts/ceatTyre.js
@@ -12,8 +12,11 @@ const CameraInfo = require("CameraInfo");
 let tapON = false;
 let touchON = false;
 let viewARON = false;
+let backCameraON = false;
+let screenViewArON = false;
 
-export const Diagnostics = require('Diagnostics');
+let featureSelected = 0;
+
 (async function () {
 
   const canvas = await Scene.root.findFirst("canvas");
@@ -27,28 +30,52 @@ export const Diagnostics = require('Diagnostics');
   const cancelBtn = await Scene.root.findFirst("cancelBtn");
 
   const TouchPoints = [
+    await Scene.root.findFirst("touchPoint1"),
+    await Scene.root.findFirst("touchPoint2"),
     await Scene.root.findFirst("touchPoint3"),
     await Scene.root.findFirst("touchPoint4"),
+    await Scene.root.findFirst("touchPoint5"),
+    await Scene.root.findFirst("touchPoint6"),
   ]
   const icons = [
+    await Scene.root.findFirst("icon1"),
+    await Scene.root.findFirst("icon2"),
     await Scene.root.findFirst("icon3"),
     await Scene.root.findFirst("icon4"),
+    await Scene.root.findFirst("icon5"),
+    await Scene.root.findFirst("icon6"),
   ]
   const iconsText = [
+    await Scene.root.findFirst("icontext1"),
+    await Scene.root.findFirst("icontext2"),
     await Scene.root.findFirst("icontext3"),
     await Scene.root.findFirst("icontext4"),
+    await Scene.root.findFirst("icontext5"),
+    await Scene.root.findFirst("icontext6"),
   ]
 
   const iconMat = [
+    await MS.findFirst('iconMat1'),
+    await MS.findFirst('iconMat2'),
     await MS.findFirst('iconMat3'),
     await MS.findFirst('iconMat4'),
+    await MS.findFirst('iconMat5'),
+    await MS.findFirst('iconMat6'),
   ];
   const iconTextMat = [
+    await MS.findFirst('icontextMat1'),
+    await MS.findFirst('icontextMat2'),
     await MS.findFirst('icontextMat3'),
     await MS.findFirst('icontextMat4'),
+    await MS.findFirst('icontextMat5'),
+    await MS.findFirst('icontextMat6'),
   ];
   const icon_selected_texture =
     [
+      await TS.findFirst('icon3_selected'),
+      await TS.findFirst('icon4_selected'),
+      await TS.findFirst('icon3_selected'),
+      await TS.findFirst('icon4_selected'),
       await TS.findFirst('icon3_selected'),
       await TS.findFirst('icon4_selected'),
     ];
@@ -57,10 +84,18 @@ export const Diagnostics = require('Diagnostics');
     [
       await TS.findFirst('icon3_unselected'),
       await TS.findFirst('icon4_unselected'),
+      await TS.findFirst('icon3_unselected'),
+      await TS.findFirst('icon4_unselected'),
+      await TS.findFirst('icon3_unselected'),
+      await TS.findFirst('icon4_unselected'),
     ];
 
   const icontext_selected_texture =
     [
+      await TS.findFirst('icontext3_selected'),
+      await TS.findFirst('icontext4_selected'),
+      await TS.findFirst('icontext3_selected'),
+      await TS.findFirst('icontext4_selected'),
       await TS.findFirst('icontext3_selected'),
       await TS.findFirst('icontext4_selected'),
     ];
@@ -69,15 +104,27 @@ export const Diagnostics = require('Diagnostics');
     [
       await TS.findFirst('icontext3_unselected'),
       await TS.findFirst('icontext4_unselected'),
+      await TS.findFirst('icontext3_unselected'),
+      await TS.findFirst('icontext4_unselected'),
+      await TS.findFirst('icontext3_unselected'),
+      await TS.findFirst('icontext4_unselected'),
     ];
   const popup_texture =
     [
+      await TS.findFirst('icon3_popup'),
+      await TS.findFirst('icon4_popup'),
+      await TS.findFirst('icon3_popup'),
+      await TS.findFirst('icon4_popup'),
       await TS.findFirst('icon3_popup'),
       await TS.findFirst('icon4_popup'),
     ];
   const tyre_with_hotspot =
     [
       await TS.findFirst('tyre_with_hotspot 0'),
+      await TS.findFirst('tyre_with_hotspot 1'),
+      await TS.findFirst('tyre_with_hotspot 2'),
+      await TS.findFirst('tyre_with_hotspot 1'),
+      await TS.findFirst('tyre_with_hotspot 2'),
       await TS.findFirst('tyre_with_hotspot 1'),
       await TS.findFirst('tyre_with_hotspot 2'),
     ];
@@ -88,15 +135,27 @@ export const Diagnostics = require('Diagnostics');
   popUp.transform.scaleX = Animation.animate(driver, samplerLinear);
   popUp.transform.scaleY = Animation.animate(driver, samplerLinear);
 
+  const planeTracker = await Scene.root.findFirst('planeTracker0');
+  const instPlace = await Scene.root.findFirst("instPlace");
+  const inst360 = await Scene.root.findFirst("inst360");
+
   const funStart = () => {
     tyreMat.diffuse = tyre_with_hotspot[0];
     TouchPoints.map((touchpoint) => {
       touchpoint.hidden = true;
     })
+
+    icons.map((icon, ind) => {
+      iconMat[ind].diffuse = icon_unselected_texture[ind]
+      iconTextMat[ind].diffuse = icontext_unselected_texture[ind]
+      icon.hidden = false;
+    })
     popUp.hidden = true;
     cancelBtn.hidden = true;
     tapON = true;
     viewARON = true;
+    instPlace.hidden = true;
+    inst360.hidden = true;
   }
 
   funStart();
@@ -114,30 +173,47 @@ export const Diagnostics = require('Diagnostics');
 
   TG.onTap(cancelBtn).subscribe(function (gesture) {
     cancelBtn.hidden = true;
-    driver.reverse();
 
+    driver.reverse();
     Time.setTimeout(() => {
+      TouchPoints[featureSelected].hidden = false;
+      tyreMat.diffuse = tyre_with_hotspot[featureSelected + 1];
       popUp.hidden = true;
       tapON = true;
     }, 300);
 
   });
 
-  TouchPoints.map((touchPoint, ind) => {
-    TG.onTap(touchPoint).subscribe(function (gesture) {
-      if (touchON) {
-        tapON = false;
-        d.log(ind);
-        popupMat.diffuse = popup_texture[ind];
-        popUp.hidden = false;
-        cancelBtn.hidden = false;
-        driver.reset();
-        driver.start();
-      }
-    });
-  })
+  // TouchPoints.map((touchPoint, ind) => {
+  //   TG.onTap(touchPoint).subscribe(function (gesture) {
+  //     if (touchON) {
+  //       featureSelected = ind;
+  //       tapON = false;
+  //       TouchPoints[ind].hidden = true;
+  //       popupMat.diffuse = popup_texture[ind];
+  //       popUp.hidden = false;
+  //       cancelBtn.hidden = false;
+  //       tyreMat.diffuse = tyre_with_hotspot[0];
+  //       driver.reset();
+  //       driver.start();
+  //       d.log(ind);
+  //     }
+  //   });
+  // })
 
+  const touchPopUp = (ind) => {
+    featureSelected = ind;
+    tapON = false;
+    TouchPoints[ind].hidden = true;
+    popupMat.diffuse = popup_texture[ind];
+    popUp.hidden = false;
+    cancelBtn.hidden = false;
+    tyreMat.diffuse = tyre_with_hotspot[0];
+    driver.reset();
+    driver.start();
+  }
 
+  let timer = Time.setTimeout(() => { }, 100);
   const showSelectedIcon = (iconNum) => {
     for (let i = 0; i < icons.length; i++) {
       if (i == iconNum) {
@@ -145,6 +221,11 @@ export const Diagnostics = require('Diagnostics');
         iconTextMat[i].diffuse = icontext_selected_texture[i];
         iconsText[i].transform.y = iconsText[i].transform.y.pinLastValue() - 12
         TouchPoints[i].hidden = false;
+        Time.clearTimeout(timer);
+        timer = Time.setTimeout(() => {
+          touchPopUp(i);
+        }, 1000)
+
       } else {
         iconMat[i].diffuse = icon_unselected_texture[i];
         iconTextMat[i].diffuse = icontext_unselected_texture[i];
@@ -154,50 +235,58 @@ export const Diagnostics = require('Diagnostics');
     }
   }
 
-
-
   TG.onTap(viewARBtn).subscribe(function (gesture) {
     if (tapON && viewARON) {
-      cameraback();
       viewARON = false;
-      arView.hidden = false;
+      screenViewArON = true;
       canvas.hidden = true;
       INS.bind(true, 'flip_camera');
+      cameraback();
     }
 
   })
 
-  var planeTracker = await Scene.root.findFirst('planeTracker0');
-
-  TG.onPan().subscribe(function (gesture) {
-    planeTracker.trackPoint(gesture.location, gesture.state);
+  TG.onTap().subscribe(function (gesture) {
+    if (screenViewArON && backCameraON) {
+      screenViewArON = false;
+      planeTracker.trackPoint(gesture.location, gesture.state);
+      instPlace.hidden = false;
+      inst360.hidden = true;
+      arView.hidden = false;
+      Time.setTimeout(() => {
+        instPlace.hidden = true;
+        inst360.hidden = false;
+        Time.setTimeout(() => {
+          inst360.hidden = true;
+        }, 4000);
+      }, 2000);
+    }
   });
 
-  // onPinch
-  let lastScaleX1, lastScaleY1, lastScaleZ1, evalue;
-  let zoomUpON = true;
-  let zoomDownON = true;
-  TG.onPinch().subscribe(function (gesture) {
+  // // onPinch
+  // let lastScaleX1, lastScaleY1, lastScaleZ1, evalue;
+  // let zoomUpON = true;
+  // let zoomDownON = true;
+  // TG.onPinch().subscribe(function (gesture) {
 
-    lastScaleX1 = arView.transform.scaleX.pinLastValue();
-    lastScaleY1 = arView.transform.scaleY.pinLastValue();
-    lastScaleZ1 = arView.transform.scaleZ.pinLastValue();
+  //   lastScaleX1 = arView.transform.scaleX.pinLastValue();
+  //   lastScaleY1 = arView.transform.scaleY.pinLastValue();
+  //   lastScaleZ1 = arView.transform.scaleZ.pinLastValue();
 
-    gesture.scale.monitor().subscribe(function (e) {
-      evalue = e.newValue;
-      if ((zoomUpON && evalue > 1) || (zoomDownON && evalue < 1)) {
+  //   gesture.scale.monitor().subscribe(function (e) {
+  //     evalue = e.newValue;
+  //     if ((zoomUpON && evalue > 1) || (zoomDownON && evalue < 1)) {
 
-        arView.transform.scaleX = Reactive.mul(lastScaleX1, gesture.scale);
-        arView.transform.scaleY = Reactive.mul(lastScaleY1, gesture.scale);
-        arView.transform.scaleZ = Reactive.mul(lastScaleZ1, gesture.scale);
-      };
-    });
-  });
-  TG.onRotate().subscribe(function (gesture) {
-    let lastRotationY1 = arView.transform.rotationZ.pinLastValue();
-    arView.transform.rotationZ = Reactive.add(lastRotationY1, Reactive.mul(-1, gesture.rotation));
-  });
-
+  //       arView.transform.scaleX = Reactive.mul(lastScaleX1, gesture.scale);
+  //       arView.transform.scaleY = Reactive.mul(lastScaleY1, gesture.scale);
+  //       arView.transform.scaleZ = Reactive.mul(lastScaleZ1, gesture.scale);
+  //     };
+  //   });
+  // });
+  // TG.onRotate().subscribe(function (gesture) {
+  //   let lastRotationY1 = arView.transform.rotationZ.pinLastValue();
+  //   arView.transform.rotationZ = Reactive.add(lastRotationY1, Reactive.mul(1, gesture.rotation));
+  // });
 
   CameraInfo.captureDevicePosition.monitor().subscribe(function (e) {
     if (e.newValue == "BACK") {
@@ -212,10 +301,12 @@ export const Diagnostics = require('Diagnostics');
       let isBackCam = CameraInfo.captureDevicePosition.eq("BACK").pinLastValue();
       if (!isBackCam) {
         tapON = true;
-        INS.bind(true, 'choose_an_option');
-        Time.setTimeout(() => {
-          INS.bind(false, 'choose_an_option');
-        }, 4000)
+        INS.bind(false, 'flip_camera');
+
+        // INS.bind(true, 'choose_an_option');
+        // Time.setTimeout(() => {
+        //   INS.bind(false, 'choose_an_option');
+        // }, 4000)
         Time.clearInterval(cameraflip);
       } else {
         tapON = false;
@@ -230,6 +321,8 @@ export const Diagnostics = require('Diagnostics');
       if (isBackCam) {
         INS.bind(false, 'flip_camera');
         Time.clearInterval(cameraflip);
+        instPlace.hidden = false;
+        backCameraON = true;
       } else {
         INS.bind(true, 'flip_camera');
       }
